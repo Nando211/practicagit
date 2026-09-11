fa80ccc2f115d50d1dc149e6727ea328592b5148
Nando <nandomontes211@gmail.com>
2026-09-11 15:55:03 -0500

    aviso cuando no colocan numeros y cuando dividen entre 0

--- calculadora.js
+++ calculadora.js
@@ -6,19 +6,26 @@ function restar(x, y) {
   return x - y;
 }
 
-function multiplicar(x,y){
-    return x * y;
-
+function multiplicar(x, y) {
+  return x * y;
 }
 
-function dividir (x , y){
-    return x / y;
-
+function dividir(x, y) {
+  if (y === 0) {
+    return "no se puede dividir entre cero";
+  }
+  return x / y;
 }
+
 const x = Number(process.argv[2]);
 const y = Number(process.argv[3]);
 
-console.log("Suma:", sumar(x, y));
-console.log("Resta:", restar(x, y));
-console.log("Multiplicación:", multiplicar(x, y));
-console.log("División:", dividir(x, y));
+if (Number.isNaN(x) || Number.isNaN(y)) {
+  console.log("Tienes que darme dos números.");
+  console.log("Ejemplo: node calculadora.js 20 4");
+} else {
+  console.log("Suma:", sumar(x, y));
+  console.log("Resta:", restar(x, y));
+  console.log("Multiplicación:", multiplicar(x, y));
+  console.log("División:", dividir(x, y));
+}
